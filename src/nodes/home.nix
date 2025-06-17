c2211c3af00b2f79dbd9d6fdec527243097000ec
Anillc <i@anillc.cn>
2025-06-18 06:29:36 +0800

    add home

---
+++ src/nodes/home.nix
@@ -0,0 +1,36 @@
+{ inputs, modules, ... }: {
+  infra.nodes.home = {
+    id = 3;
+    address = "192.168.1.7";
+    singbox.enable = true;
+
+    modules = (modules "nixos") ++ [ ({ modulesPath, ... }: {
+      # hardware
+      imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+      boot.kernelModules = [ "kvm-intel" ];
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+      boot.swraid = {
+        enable = true;
+        mdadmConf = ''
+          MAILADDR root
+          ARRAY /dev/md0 level=raid5 num-devices=3 metadata=1.2 UUID=a3b1c844:9d8d6537:93a2d397:500029e1
+             devices=/dev/sda1,/dev/sdb1,/dev/sdc1
+        '';
+      };
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/f1021e42-d516-450d-8f93-34d48205c770";
+        fsType = "xfs";
+      };
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/9D8B-5261";
+        fsType = "vfat";
+      };
+      fileSystems."/data" = {
+        device = "/dev/md0";
+        fsType = "xfs";
+      };
+    }) ];
+  };
+}
